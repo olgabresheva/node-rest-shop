0e8b89649622bcb3fc5a47337a6ae1f0690dc545
olgab <olgabresheva7@gmail.com>
2020-09-15 02:43:12 -0700

    added checkAuth to user router

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const userControllers = require('../controllers/user');
 
 router.post('/signup', userControllers.user_create);
 router.post('/login', userControllers.user_login);
-router.delete('/:userId', userControllers.user_delete);
+router.delete('/:userId', checkAuth, userControllers.user_delete);
 router.get('/', userControllers.user_get_all);
 router.get('/:userId', userControllers.user_get_byID);
 
